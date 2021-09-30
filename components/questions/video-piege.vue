c5bf312e7b526ed065b0038441cf8b7bb904d98c
Boulanouar <b.baghdadi93@gmail.com>
2021-09-30 12:10:30 +0200

    page piège

---
+++ components/questions/video-piege.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="w-full py-12 px-24 grid justify-items-center">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/8f1FUn3HK6U" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+</template>
+<script>
+export default {
+    name: 'video-piege'
+}
+</script>
---
+++ layouts/default-questions.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="min-h-screen bg-gray-100">
+  <div class="w-full py-6 px-24 grid justify-items-center">
+    <Logo :dark="!primary" />
+  </div>
+    <Nuxt /> 
+</div>
+</template>
+<script>
+import Logo from '~/components/layouts/Logo.vue'
+
+export default {
+  components: { Logo },
+    name: "default-questions", 
+    
+}
+</script>
