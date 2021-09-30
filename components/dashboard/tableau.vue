8a8745eeca2dea9a344d7b0ecdedcad0f7eeb840
Boulanouar <b.baghdadi93@gmail.com>
2021-09-30 16:25:09 +0200

    campaign

---
+++ components/dashboard/tableau.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+<p class="text-lg text-center font-bold m-5">Contacts associés à la campagne</p>
+<table class="rounded-t-lg m-5 w-5/6 mx-auto bg-gray-200 text-gray-800">
+  <tr class="text-left border-b-2 border-gray-300">
+    <th class="px-4 py-3">Nom</th>
+    <th class="px-4 py-3">Prenom</th>
+    <th class="px-4 py-3">E-mail</th>
+    <th class="px-4 py-3">Step 1</th>
+    <th class="px-4 py-3">Step 2</th>
+    <th class="px-4 py-3">Step 3</th>
+  </tr>
+  
+  <tr class="bg-gray-100 border-b border-gray-200">
+    <td class="px-4 py-3">Jill</td>
+    <td class="px-4 py-3">Smith</td>
+    <td class="px-4 py-3">jillsmith@epsi.fr</td>
+    <td class="px-4 py-3">Yes</td>
+    <td class="px-4 py-3">No</td>
+    <td class="px-4 py-3">No</td>
+  </tr> 
+<!-- each row -->
+  <tr class="bg-gray-100 border-b border-gray-200">
+    <td class="px-4 py-3">Alain</td>
+    <td class="px-4 py-3">Proviste</td>
+    <td class="px-4 py-3">alainproviste@epsi.fr</td>
+    <td class="px-4 py-3">Yes</td>
+    <td class="px-4 py-3">Yes</td>
+    <td class="px-4 py-3">No</td>
+  </tr> 
+<!-- each row -->
+  <tr class="bg-gray-100 border-b border-gray-200">
+    <td class="px-4 py-3">Justin</td>
+    <td class="px-4 py-3">Bridou</td>
+    <td class="px-4 py-3">justinbridou@epsi.fr</td>
+    <td class="px-4 py-3">Yes</td>
+    <td class="px-4 py-3">Yes</td>
+    <td class="px-4 py-3">No</td>
+  </tr> 
+<!-- each row -->
+
+</table>
+
+<!-- classic design -->
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'tableau'
+}
+</script>
--- pages/dashboard/campaign/_id.vue
+++ pages/dashboard/campaign/_id.vue
@@ -1,5 +1,18 @@
 <template>
     <div>
-        coucou
+        <dropdown/>
+        <stats/>
+        <tableau/>
     </div>
 </template>
+
+<script>
+import Stats from '~/components/dashboard/stats.vue';
+import Tableau from '~/components/dashboard/tableau.vue';
+import Dropdown from '~/components/dashboard/dropdown.vue';
+
+export default {
+layout: "dashboard",
+  components: {Stats, Tableau, Dropdown, },
+}
+</script>
