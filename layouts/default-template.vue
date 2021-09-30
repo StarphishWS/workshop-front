e41c6786d3beaaa4508dfed3766cc116d542f82b
Corentin Lissillour <corentin.lissillour@hotmail.fr>
2021-09-30 16:04:40 +0200

    WIP google template

---
+++ layouts/default-template.vue
@@ -0,0 +1,13 @@
+<template>
+<div class="min-h-screen">
+  <div class="w-full h-screen flex justify-center items-center">
+    <Nuxt /> 
+  </div>
+</div>
+</template>
+<script>
+
+export default {
+    
+}
+</script>
